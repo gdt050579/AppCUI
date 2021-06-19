81f0dc6573c195b469483309ed1ff1367a4a3774
Gavrilut Dragos <dgavrilut@bitdefender.com>
2021-06-19 10:38:00 +0300

    Tester and solutions files

--- AppCUI/AppCUI.cpp
+++ AppCUI/AppCUI.cpp
@@ -1,4 +1,17 @@
-// AppCUI.cpp : Defines the exported functions for the DLL application.
+#include "Windows.h"
 
-
-
+BOOL APIENTRY DllMain(HMODULE hModule,
+    DWORD  ul_reason_for_call,
+    LPVOID lpReserved
+)
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
--- Tester/Tester.cpp
+++ Tester/Tester.cpp
@@ -1,8 +1,15 @@
-
 #include <iostream>
+#include "../AppCUI/include/AppCUI.h"
+
+using namespace AppCUI;
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+    Application::Init(Application::Flags::HAS_COMMANDBAR);
+    Application::SetCommand(Input::Key::F1, "Run", 100);
+    Application::SetCommand(Input::Key::F2, "Debug", 100);
+    Application::SetCommand(Input::Key::F3, "Run as admin", 100);
+    Application::Run();
+    Application::Close();
 }
 
