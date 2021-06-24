53c44eaf8e9a9e7c060ff40efbc8480c9bbb35fc
Gavrilut Dragos <dgavrilut@bitdefender.com>
2021-06-24 22:37:04 +0300

    Panel control / container

---
+++ AppCUI/src/Controls/Panel.cpp
@@ -0,0 +1,35 @@
+#include "../../include/ControlContext.h"
+
+using namespace AppCUI::Controls;
+using namespace AppCUI::Console;
+using namespace AppCUI::Input;
+
+#define PANEL_ATTR_BORDER	1024
+
+bool Panel::Create(Control *parent, const char * text, const char * layout)
+{
+	CONTROL_INIT_CONTEXT(ControlContext);
+	CHECK(Init(parent, text, layout,  false), false, "Failed to create panel !");
+	CREATE_CONTROL_CONTEXT(this, Members, false);
+	Members->Flags = GATTR_VISIBLE | GATTR_ENABLE | PANEL_ATTR_BORDER;
+	CHECK(SetMargins(1,1,1,1), false, "Failed to set margins !");
+	return true;
+}
+bool Panel::Create(Control *parent, const char * layout)
+{
+	CONTROL_INIT_CONTEXT(ControlContext);
+	CHECK(Init(parent, "", layout, false), false, "Failed to create panel !");
+	CREATE_CONTROL_CONTEXT(this, Members, false);
+	Members->Flags = GATTR_VISIBLE | GATTR_ENABLE;
+	return true;
+}
+void Panel::Paint(Console::Renderer & renderer)
+{
+	CREATE_CONTROL_CONTEXT(this, Members, );
+    renderer.Clear(' ', Members->Cfg->Panel.NormalColor);
+	if (Members->Flags & PANEL_ATTR_BORDER)
+	{
+        renderer.DrawRectWidthHeight(0, 0, Members->Layout.Width, Members->Layout.Height, Members->Cfg->Panel.NormalColor, false);
+        renderer.WriteSingleLineText(3, 0, Members->Text, Members->Cfg->Panel.TextColor, Members->Text.Len());
+	}
+}
