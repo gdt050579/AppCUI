670494c0c82d9224aa9cff11993692231a9e57ba
Gavrilut Dragos <dgavrilut@bitdefender.com>
2021-06-26 09:15:15 +0300

    HotKey support for Label

--- AppCUI/src/Controls/Label.cpp
+++ AppCUI/src/Controls/Label.cpp
@@ -11,8 +11,8 @@ bool Label::Create(Control *parent, const char * text, const char * layout)
     CREATE_CONTROL_CONTEXT(this, Members, false);
     Members->Layout.MinHeight = 1;
     Members->Layout.MinWidth = 1;
-	CHECK(Init(parent, text, layout,  false), false, "Failed to create panel !");
-	
+	CHECK(Init(parent, text, layout,  true), false, "Failed to create label !");
+    Members->HotKey = AppCUI::Input::Key::None; // A label can draw a hot key, but does not have an associated one
 	Members->Flags = GATTR_ENABLE | GATTR_VISIBLE ;
 	return true;
 }
@@ -20,9 +20,10 @@ bool Label::Create(Control *parent, const char * text, const char * layout)
 void Label::Paint(Console::Renderer & renderer)
 {
 	CREATE_CONTROL_CONTEXT(this, Members, );
-    WriteCharacterBufferParams params(WriteCharacterBufferFlags::OVERWRITE_COLORS);
+    WriteCharacterBufferParams params(WriteCharacterBufferFlags::OVERWRITE_COLORS | WriteCharacterBufferFlags::HIGHLIGHT_HOTKEY);
     params.Color = Members->Cfg->Label.NormalColor;
     params.HotKeyColor = Members->Cfg->Label.HotKeyColor;
+    params.HotKeyPosition = Members->HotKeyOffset;
     if (Members->Layout.Height == 1) {
         params.Flags |= WriteCharacterBufferFlags::SINGLE_LINE;
     } else {
