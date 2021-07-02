2359a996dd254ffcbe43b3821d5ee9a912388ada
Denis Rozimovschii <rozimovschii.denis@gmail.com>
2021-07-02 12:28:46 +0300

    Updated message for users that don't have ncurses, updated cmake.yml for macos-test

--- AppCUI/src/OS/Unix/ConsoleDefault.cpp
+++ AppCUI/src/OS/Unix/ConsoleDefault.cpp
@@ -18,10 +18,23 @@
 
 using namespace AppCUI::Internal;
 
+Console::Console()
+{
+    std::cout << "[+] I am a default console and I'll crash now" << std::endl;
+    std::cout << "[+] If you see me, you probably don't have ncurses on your system" << std::endl;
+    std::cout << "[+] The build worked for CI and legacy purposes" << std::endl;
+    std::cout << std::endl;
+    std::cout << "[+] So...what do?" << std::endl;
+    std::cout << "[+] Try telling cmake the path to your ncurses" << std::endl;
+    std::cout << "[+] This is done by setting the -DCMAKE_PREFIX_PATH during configure" << std::endl;
+    std::cout << "[+] For example: cmake -DCMAKE_PREFIX_PATH=/opt/homebrew/Cellar/ncurses/6.2" << std::endl;
+    std::cout << "[+] Also rm -rf the build folder and reconfigure again to remove the cache" << std::endl;
+    std::cout << std::endl;
+}
 
 bool Console::OnInit()
 {
-
+    return true;
 }
 
 void Console::OnUninit() 
