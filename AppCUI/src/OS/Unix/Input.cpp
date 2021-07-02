7404d86d53e1c6e7ea130c905028edfb41e7ed59
Denis Rozimovschii <rozimovschii.denis@gmail.com>
2021-07-03 01:25:14 +0300

    Added overly complicated docs for installing the proper terminal

--- AppCUI/src/OS/Unix/Input.cpp
+++ AppCUI/src/OS/Unix/Input.cpp
@@ -1,37 +1,7 @@
 #include "os.h"
 #include <ncurses.h>
 
-/**
-    Short (not) unix terminal and terminal introduction
-    1. https://en.wikipedia.org/wiki/Terminal_emulator
-    2. https://en.wikipedia.org/wiki/Xterm (the default terminal emulator on most systems)
-    3. https://en.wikipedia.org/wiki/ANSI_escape_code
-    4. https://en.wikipedia.org/wiki/Termcap
-    5. https://en.wikipedia.org/wiki/Terminfo
-    6. https://tldp.org/HOWTO/Text-Terminal-HOWTO-16.html
-
-    The basic gist is as follows:
-    
-    You have 3 parts of the application:
-    1. The terminal (usually a program that runs by itself and can run other programs)
-    2. The application that wants to output/draw something on the terminal it runs on
-    3. A helper library like ncurses
-
-    What usually happens:
-    1. You init ncurses using its functions
-    2. Ncurses will find out the terminal you're running from (for example xterm-256color) and check its capabilities,
-       for example: can it display 256 colors or 8? how many color pairs can it store? what mouse events does it send.
-    3. Along with capabilities, every terminal also defines what control sequences to send so that it can perform
-       those capabilities
-       For example, to move the cursor to a certain line L and column C, your program has to send "\033[<L>;<C>H"
-    4. But you don't have to do it manually, ncurses does it for you, by reading your terminal capabilities file
-       and finding all kinds of commands using their names.
-       For example "clear=\E[H\E[​2J,", which means that for ncurses to do a screen clear, it can send a
-       "\E[H\E[​2J" control sequence to the terminal and it will be interpreted as a clear command. The command
-       names are predefined by a standard, but are optional to be implemented by terminals. If a terminal doesn't define
-       a clear capability, it will not be executed when you use ncurses
-*/
-
+// See docs/TERMINAL.md
 
 using namespace AppCUI::Internal;
 
--- README.md
+++ README.md
@@ -13,3 +13,30 @@ This is done by setting the `-DCMAKE_PREFIX_PATH`
 For example: `cmake -DCMAKE_PREFIX_PATH=/opt/homebrew/Cellar/ncurses/6.2`
 
 Also `rm -rf` the build folder and reconfigure again to remove the cache
+
+## Setting up a proper terminal
+
+Details can be found in `docs/terminal/TERMINAL.md`
+
+We would need the terminal capabilities for `256 colors` and the terminal capabilities for proper mouse events like
+press, released and also position reporting on every more (for drag functionality).
+
+The color part can be implemented by `xterm-256color` which comes by default in `Debian` and most terminals on unix systems (or can be setup if it's not default).
+
+For mouse events we can use `xterm-1003` capabilities, the `kmous`, `XM` and `xm` ones, to be precise
+
+Thus, we have crafted the following terminal capabilities file - `docs/terminal/xterm-256color-mouse`
+
+### Linux 
+
+1. Run the terminfo compiler `tic -x docs/terminal/xterm-256color-mouse`
+2. Set the current terminal `export TERM=xterm-256color-mouse`
+    * For persistence, the following command can be added to your shell configuration
+3. Run the program!
+
+### MacOS
+
+The same steps as on linux, but ensure you're using the proper `tic`. After installing `brew install ncurses`, the
+`tic` or other terminal capabilities related programs should be run from the `bin` folder of ncurses so it can find the
+capability files when running the program. For example: `miniconda` also ships the `tic` binary, which compiles the 
+capabilities provided, but `ncurses` doesn't find them.
