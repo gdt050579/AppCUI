a6638850779c47aba7b74bedf0530337f4f515b2
Denis Rozimovschii <rozimovschii.denis@gmail.com>
2021-07-03 19:09:44 +0300

    Updated terminal doc

---
+++ docs/terminal/term_test.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <iostream>
+#include <term.h>
+/*
+    https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797
+*/
+int main() 
+{
+    std::cout << "\033[1mthis is going to be bold, "
+                << "\033[0m" << std::endl // Reset formatting 
+                << "\033[3mbut this will be italic, "
+                << "\033[0m" << std::endl
+                << "\033[9mthis text should be strikethrough"
+                << "\033[0m" << std::endl
+                << "\033[31m\033[47mthis text should have red foreground and white background"
+                << "\033[0m" << std::endl
+                << "this text will not be formatted";
+
+    return 0;
+}
