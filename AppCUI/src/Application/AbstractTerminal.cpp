6661a4c588ef1fbb98cf7bedc41c32709065d1af
Gavrilut Dragos <dgavrilut@bitdefender.com>
2021-07-05 09:08:57 +0300

    Switch to a terminal class that includes both console and input classes

---
+++ AppCUI/src/Application/AbstractTerminal.cpp
@@ -0,0 +1,57 @@
+#include "Internal.h"
+
+using namespace AppCUI::Internal;
+using namespace AppCUI::Console;
+
+AbstractTerminal::AbstractTerminal()
+{
+    this->LastCursorVisibility = false;
+    this->LastCursorX = 0xFFFFFFFF;
+    this->LastCursorY = 0xFFFFFFFF;
+    this->Inited = false;
+}
+AbstractTerminal::~AbstractTerminal()
+{
+}
+
+bool AbstractTerminal::Init()
+{
+    return OnInit();
+}
+void AbstractTerminal::Uninit()
+{    
+    // clear up current buffer
+    this->ScreenCanvas.ClearEntireSurface(' ', COLOR(Color::Silver, Color::Black));
+    // copy the original buffer
+    this->ScreenCanvas.DrawCanvas(0, 0, this->OriginalScreenCanvas);
+    // restore the original buffer
+    this->OnFlushToScreen();
+    // Copy cursor position
+    if (this->OriginalScreenCanvas.GetCursorVisibility())
+        this->ScreenCanvas.SetCursor(this->OriginalScreenCanvas.GetCursorX(), this->OriginalScreenCanvas.GetCursorY());
+    else
+        this->ScreenCanvas.HideCursor();
+    // restore the original cursor position
+    this->OnUpdateCursor();
+    // OS specific On-unit
+    this->OnUninit();
+}
+void AbstractTerminal::Update()
+{
+    this->OnFlushToScreen();
+    
+    if ((this->ScreenCanvas.GetCursorVisibility() != this->LastCursorVisibility) ||
+        (this->ScreenCanvas.GetCursorX() != this->LastCursorX) ||
+        (this->ScreenCanvas.GetCursorY() != this->LastCursorY))
+    {
+        if (this->OnUpdateCursor())
+        {
+            // update last cursor information
+            this->LastCursorX = this->ScreenCanvas.GetCursorX();
+            this->LastCursorY = this->ScreenCanvas.GetCursorY();
+            this->LastCursorVisibility = this->ScreenCanvas.GetCursorVisibility();
+        }
+    }
+}
+
+
