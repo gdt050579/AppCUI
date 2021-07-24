edd2c8412d1216fb30c08bf9b711bfaa30dbdeea
Gavrilut Dragos <dgavrilut@bitdefender.com>
2021-07-24 21:46:06 +0300

    Prepare ini parser object (2)

--- AppCUI/src/Utils/ini_parser_char_type_builder.py
+++ AppCUI/src/Utils/ini_parser_char_type_builder.py
@@ -5,7 +5,7 @@ def GetCharType(i):
 	if i>=ord('0') and i<=ord('9'): return "CHAR_TYPE_NUMBER"
 	if (i==ord('_')) or (i==ord('.')): return "CHAR_TYPE_WORD"
 	if (i==32) or (i==ord('\t')): return "CHAR_TYPE_SPACE"
-	if (i==ord('\n')) or (i==ord('\r')): return "CHAR_TYPE_SPACE"
+	if (i==ord('\n')) or (i==ord('\r')): return "CHAR_TYPE_NEW_LINE"
 	if (i==ord('"')) or (i==ord('\'')): return "CHAR_TYPE_STRING"
 	if (i==ord(';')) or (i==ord('#')): return "CHAR_TYPE_COMMENT"
 	if (i==ord('=')): return "CHAR_TYPE_EQ"
