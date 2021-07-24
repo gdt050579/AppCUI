75ebbbcedfa8f37bbc57aa293fb54ad163389833
Gavrilut Dragos <dgavrilut@bitdefender.com>
2021-07-24 19:28:23 +0300

    fix for AppCUI

--- AppCUI/include/AppCUI.h
+++ AppCUI/include/AppCUI.h
@@ -1391,10 +1391,8 @@ namespace AppCUI
 
         EXPORT Config*      GetAppConfig();
         [[nodiscard("Please check the return of the Init function. If false, AppCUI has not been initialized properly")]]
-        //[[nodiscard]]
         EXPORT bool         Init(Application::InitializationFlags flags = Application::InitializationFlags::NONE);
         [[nodiscard("Please check the return of the Init function. If false, AppCUI has not been initialized properly")]]
-        //[[nodiscard]]
         EXPORT bool         Init(unsigned int width, unsigned int height, Application::InitializationFlags flags = Application::InitializationFlags::NONE);
         EXPORT bool         Run();
         EXPORT bool         AddWindow(AppCUI::Controls::Window * wnd);
