44ba2ef193ae1356f6b5c4557746da90e15a6816
Gavrilut Dragos <dgavrilut@bitdefender.com>
2021-07-31 23:49:27 +0300

    OS file for UNIX

---
+++ AppCUI/src/OS/Unix/OS.cpp
@@ -0,0 +1,11 @@
+#include "Internal.hpp"
+
+using namespace AppCUI::OS;
+
+
+
+void AppCUI::OS::GetSpecialFolders(std::vector<std::pair<std::string, std::filesystem::path>>& specialFolderLists)
+{
+    specialFolderLists.clear();
+    // to be implemented
+}
