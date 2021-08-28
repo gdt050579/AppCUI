701c0655ba4e81c66d264d489ada5404419cfa76
Gavrilut Dragos <dgavrilut@bitdefender.com>
2021-08-29 00:29:24 +0300

    Change some GetPixel signatures from Image object

--- AppCUI/include/AppCUI.hpp
+++ AppCUI/include/AppCUI.hpp
@@ -1266,8 +1266,8 @@ namespace Graphics
               unsigned char Green,
               unsigned char Blue,
               unsigned char Alpha = 255);
-        unsigned int GetPixel(unsigned int x, unsigned int y, unsigned int invalidIndexValue = 0);
-        bool GetPixel(unsigned int x, unsigned int y, unsigned int& color);
+        unsigned int GetPixel(unsigned int x, unsigned int y, unsigned int invalidIndexValue = 0) const;
+        bool GetPixel(unsigned int x, unsigned int y, unsigned int& color) const;
         bool Clear(unsigned int color);
         bool Clear(const Color color);
         inline unsigned int GetWidth() const
