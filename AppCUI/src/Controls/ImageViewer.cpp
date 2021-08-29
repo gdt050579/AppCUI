70a16b427e132fb9b0946db03392f22635fc500d
Gavrilut Dragos <dgavrilut@bitdefender.com>
2021-08-29 09:51:58 +0300

    Colors map

--- AppCUI/src/Controls/ImageViewer.cpp
+++ AppCUI/src/Controls/ImageViewer.cpp
@@ -3,7 +3,35 @@
 using namespace AppCUI::Controls;
 using namespace AppCUI::Graphics;
 
-
+Color _color_map_[] = {
+    /* 0*/ Color::Black,     // (0, 0, 0)
+    /* 1*/ Color::DarkBlue,  // (0, 0, 1)
+    /* 2*/ Color::Blue,      // (0, 0, 2)
+    /* 3*/ Color::DarkGreen, // (0, 1, 0)
+    /* 4*/ Color::Teal,      // (0, 1, 1)
+    /* 5*/ Color::Teal,      // (0, 1, 2) [Aprox]
+    /* 6*/ Color::Green,     // (0, 2, 0)
+    /* 7*/ Color::Teal,      // (0, 2, 1) [Aprox]
+    /* 8*/ Color::Aqua,      // (0, 2, 2)
+    /* 9*/ Color::DarkRed,   // (1, 0, 0)
+    /*10*/ Color::Magenta,   // (1, 0, 1)
+    /*11*/ Color::Magenta,   // (1, 0, 2) [Aprox]
+    /*12*/ Color::Olive,     // (1, 1, 0)
+    /*13*/ Color::Gray,      // (1, 1, 1)
+    /*14*/ Color::Gray,      // (1, 1, 2) [Aprox]
+    /*15*/ Color::Olive,     // (1, 2, 0) [Aprox]
+    /*16*/ Color::Gray,      // (1, 2, 1) [Aprox]
+    /*17*/ Color::Silver,    // (1, 2, 2) [Aprox]
+    /*18*/ Color::Red,       // (2, 0, 0)
+    /*19*/ Color::Magenta,   // (2, 0, 1) [Aprox]
+    /*20*/ Color::Pink,      // (2, 0, 2)
+    /*21*/ Color::Olive,     // (2, 1, 0) [Aprox]
+    /*22*/ Color::Gray,      // (2, 1, 1) [Aprox]
+    /*23*/ Color::Silver,    // (2, 1, 2) [Aprox]
+    /*24*/ Color::Yellow,    // (2, 2, 0)
+    /*25*/ Color::Silver,    // (2, 2, 1) [Aprox]
+    /*26*/ Color::White,     // (2, 2, 2)
+};
 inline unsigned int Channel_To_Index(unsigned int rgbChannelValue)
 {
     if (rgbChannelValue <= 64)
@@ -14,32 +42,10 @@ inline unsigned int Channel_To_Index(unsigned int rgbChannelValue)
 }
 Color RGB_to_Color(unsigned int colorRGB)
 {
-    unsigned int r = Channel_To_Index(colorRGB & 0xFF);
-    unsigned int g = Channel_To_Index((colorRGB >> 8) & 0xFF);
-    unsigned int b = Channel_To_Index((colorRGB >> 16) & 0xFF);
-    // normalize
-    if ((r+g+b)==5) // one of the channels is "1", the rest are "2"
-    {
-        r = g = b = 2;
-    }
-    if ((r+g+b)==4)
-    {
-        if ((r == 1) || (g == 1) || (b==1)) // one of the channels is "2", the rest are "1"
-        {
-            r = g = b = 1;
-        }
-    }
-    // after this step, r,g,b can be a combination of "0" with either "1" or "2"
-    if ((r+g+b)<=3)
-    {
-        // darker colors
-        return static_cast<Color>(b | (g << 1) | (r << 2));
-    }
-    else
-    {
-        // lighter colors
-        return static_cast<Color>(((b >> 1) | (g) | (r << 1)) | 8);
-    }
+    unsigned int b = Channel_To_Index(colorRGB & 0xFF);  // blue channel
+    unsigned int g = Channel_To_Index((colorRGB >> 8) & 0xFF); // green channel
+    unsigned int r = Channel_To_Index((colorRGB >> 16) & 0xFF); // red channel
+    return _color_map_[r * 9 + g * 3 + b];
 }
 bool Paint_SmallBoxes(Canvas& c, const AppCUI::Graphics::Image& img)
 {
