6cdbacbebd0c006c85e5ebeb43980f6542be7ffe
Gheorghita MUTU <gheorghitamutu@gmail.com>
2021-08-29 15:43:16 +0300

    ! numeric selector - new keys need to be added in SDL as well

--- AppCUI/src/Terminal/WindowsTerminal/WindowsTerminal.cpp
+++ AppCUI/src/Terminal/WindowsTerminal/WindowsTerminal.cpp
@@ -226,6 +226,7 @@ void WindowsTerminal::BuildKeyTranslationMatrix()
     this->KeyTranslationMatrix[VK_END]    = AppCUI::Input::Key::End;
     this->KeyTranslationMatrix[VK_SPACE]  = AppCUI::Input::Key::Space;
 
+    // TODO: add these to SDL as well
     this->KeyTranslationMatrix[VK_OEM_PLUS]   = AppCUI::Input::Key::Plus;
     this->KeyTranslationMatrix[VK_OEM_COMMA]  = AppCUI::Input::Key::Comma;
     this->KeyTranslationMatrix[VK_OEM_MINUS]  = AppCUI::Input::Key::Minus;
