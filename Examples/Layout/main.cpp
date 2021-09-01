882de8af455586e8dc76f9ba57ad8cc2a991a767
Gavrilut Dragos <dgavrilut@bitdefender.com>
2021-09-02 00:01:51 +0300

    working examples

--- Examples/Layout/main.cpp
+++ Examples/Layout/main.cpp
@@ -5,7 +5,7 @@ using namespace AppCUI::Application;
 using namespace AppCUI::Controls;
 using namespace AppCUI::Graphics;
 
-class MyControl : public UserControl
+class MyControl : public AppCUI::Controls::UserControl
 {
   public:
     void Paint(AppCUI::Graphics::Renderer& renderer) override
@@ -17,10 +17,10 @@ class LayoutWin : public AppCUI::Controls::Window
 {
     MyControl m;
   public:
-    LayoutWin(std::string_view & layout)
+    LayoutWin(const std::string_view & layout)
     {
         this->Create("Test", "a:c,w:40,h:14", WindowFlags::Sizeable);
-        m.Create(this, layout);
+        m.Create(this, layout);        
     }
 };
 
@@ -32,17 +32,37 @@ class MainWin : public AppCUI::Controls::Window
     MainWin()
     {
         this->Create("Layout modex", "x:0,y:0,w:70,h:20");
-        l1.Create(
-              this,
-              "This is a simple Window example that creates a 40x20 fixed window that  will close if you click on the "
-              "[X]  button from the bottom-top corner.",
-              "x:1,y:1,w:36,h:5");
+        lst.Create(this, "x:1,y:1,w:66,h:14");
+        lst.AddColumn("Layout", TextAlignament::Left, 25);
+        lst.AddColumn("Explanation", TextAlignament::Left, 100);
+        
+        // add list of layouts
+        lst.AddItem("x:3,y:3,w:4,h:4", "A 4x4 character object that starts from (3,3)");
+        lst.AddItem("x:2,y:2,w:75%,h:75%", "An object that has 75% of its parent for width and height and starts from (2,2)");
+
+        b.Create(this, "&Show", "X:28,y:16,w:16", 1234);
+        b.SetEnabled(lst.GetCurrentItem() != InvalidItemHandle);
     }
     bool OnEvent(Control* sender, Event eventType, int controlID) override
     {
         if (eventType == Event::EVENT_WINDOW_CLOSE)
         {
             Application::Close();
+            return true;
+        }
+        if (eventType == Event::EVENT_BUTTON_CLICKED)
+        {
+            auto itm = lst.GetCurrentItem();
+            if (itm != InvalidItemHandle)
+            {
+                LayoutWin dlg((std::string)lst.GetItemText(itm, 0));
+                dlg.Show();
+                return true;
+            }
+        }
+        if (eventType == Event::EVENT_LISTVIEW_CURRENTITEM_CHANGED)
+        {
+            b.SetEnabled(lst.GetCurrentItem() != InvalidItemHandle);
             return true;
         }
         return false;
@@ -52,7 +72,7 @@ int main()
 {
     if (!Application::Init())
         return 1;
-    Application::AddWindow(new SimpleWin());
+    Application::AddWindow(new MainWin());
     Application::Run();
     return 0;
 }
