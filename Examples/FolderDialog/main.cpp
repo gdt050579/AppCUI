413c9e43a8d465883accb3055c2e5c1f25b98443
Gheorghita MUTU <gheorghitamutu@gmail.com>
2021-09-10 14:56:27 +0300

    + new folder dialog (copied over from file dialog) (2)

---
+++ Examples/FolderDialog/main.cpp
@@ -0,0 +1,70 @@
+#include "AppCUI.hpp"
+
+using namespace AppCUI;
+using namespace AppCUI::Application;
+using namespace AppCUI::Controls;
+using namespace AppCUI::Dialogs;
+
+constexpr unsigned int BUTTON_ID_SHOW_SAVE = 1;
+constexpr unsigned int BUTTON_ID_SHOW_OPEN = 2;
+
+class MyWin : public AppCUI::Controls::Window
+{
+    Button b1, b2;
+    TextField l1, l2;
+
+  public:
+    MyWin()
+    {
+        this->Create("Folder example", "a:c,w:40,h:13");
+        b1.Create(this, "&Save", "x:1,y:1,w:9", BUTTON_ID_SHOW_SAVE);
+        l1.Create(this, "", "x:12,y:1,w:24,h:4");
+        b2.Create(this, "&Open", "x:1,y:6,w:9", BUTTON_ID_SHOW_OPEN);
+        l2.Create(this, "", "x:12,y:6,w:24,h:4");
+    }
+
+    bool OnEvent(Control* sender, Event eventType, int controlID) override
+    {
+        if (eventType == Event::EVENT_WINDOW_CLOSE)
+        {
+            Application::Close();
+            return true;
+        }
+        if (eventType == Event::EVENT_BUTTON_CLICKED)
+        {
+            if (controlID == BUTTON_ID_SHOW_SAVE)
+            {
+                auto res = FileDialog::ShowSaveFileWindow(
+                      "", "Text Files:txt,Images:[jpg,jpeg,png],Documents:[pdf,doc,docx,xlsx,xls,ppt,pptx]", ".");
+                if (res.has_value())
+                    l1.SetText(res->u8string());
+                else
+                    l1.SetText("Command canceled !");
+            }
+            if (controlID == BUTTON_ID_SHOW_OPEN)
+            {
+                auto res = FileDialog::ShowOpenFileWindow(
+                      "", "Text Files:txt,Images:[jpg,jpeg,png],Documents:[pdf,doc,docx,xlsx,xls,ppt,pptx]", ".");
+                if (res.has_value())
+                    l2.SetText(res->u8string());
+                else
+                    l2.SetText("Command canceled !");
+            }
+            return true;
+        }
+        return false;
+    }
+};
+
+int main()
+{
+    if (!Application::Init())
+    {
+        return 1;
+    }
+
+    Application::AddWindow(new MyWin());
+    Application::Run();
+
+    return 0;
+}
