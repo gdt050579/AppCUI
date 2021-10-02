a77a762fb247f08c04e67617aac5ed858dcb31ec
Denis Rozimovschii <beaverden@users.noreply.github.com>
2021-10-02 22:08:36 +0300

    Special folders and FileDialog redesign (#124)
    * Added platform folders and changed file dialog design for usability
    
    * Added stack to includes for unix
    
    * Fix on windows for variable naming
    
    * Removed home folder from selection
    
    * Removed path stack until further improvements
    
    * Removed useless cast
    
    * The cast wasn't that useless after all
    
    * Bumped paths to u16 for compatibility and correct finding when u16 symbols are present
    
    * Typos and additional path check
    
    * Removed const char* operations and replaced them with u16
    
    * Updated example with russian text and extension
    
    * Better u16 transform
    
    * Added local folders back
    
    * Optimize extension search
    
    * Changed the format of filter extensions for easier parsing
    
    * Changed separator to | and added splitter for listviews
    
    * Add documentation for FileDialog usage

--- AppCUI/src/OS/Unix/OS.cpp
+++ AppCUI/src/OS/Unix/OS.cpp
@@ -1,15 +1,60 @@
 #include "Internal.hpp"
+#include "PlatformFolders.hpp"
 #include "Whereami.hpp"
+#include <sys/mount.h>
+
 using namespace AppCUI::OS;
 
-void AppCUI::OS::GetSpecialFolders(
-      std::vector<std::pair<std::string, std::filesystem::path>>& specialFolderLists,
-      SpecialFoldersType,
-      bool clearVector)
+void AppCUI::OS::GetSpecialFolders(SpecialFolderMap& specialFolders, RootsVector& roots)
 {
-    if (clearVector)
-        specialFolderLists.clear();
-    // to be implemented
+    const auto desktop = sago::getDesktopFolder();
+    if (!desktop.empty())
+    {
+        specialFolders[SpecialFolder::Desktop] = { "Desktop", desktop };
+    }
+
+    specialFolders[SpecialFolder::AppPath] = { "App Folder", GetCurrentApplicationPath().parent_path() };
+
+    const auto documents = sago::getDocumentsFolder();
+    if (!desktop.empty())
+    {
+        specialFolders[SpecialFolder::Documents]      = { "Documents", documents };
+        specialFolders[SpecialFolder::LocalDocuments] = { "Local Documents", documents };
+    }
+
+    auto downloadFolder = sago::getDownloadFolder();
+    if (downloadFolder.empty())
+    {
+        downloadFolder = sago::getDownloadFolder1();
+    }
+    if (!downloadFolder.empty())
+    {
+        specialFolders[SpecialFolder::Downloads]      = { "Downloads", downloadFolder };
+        specialFolders[SpecialFolder::LocalDownloads] = { "Local Downloads", downloadFolder };
+    }
+
+    const auto music = sago::getMusicFolder();
+    if (!music.empty())
+    {
+        specialFolders[SpecialFolder::Music]      = { "Music", music };
+        specialFolders[SpecialFolder::LocalMusic] = { "Local Music", music };
+    }
+
+    const auto pictures = sago::getPicturesFolder();
+    if (!pictures.empty())
+    {
+        specialFolders[SpecialFolder::Pictures]      = { "Pictures", pictures };
+        specialFolders[SpecialFolder::LocalPictures] = { "Local Pictures", pictures };
+    }
+
+    const auto video = sago::getVideoFolder();
+    if (!video.empty())
+    {
+        specialFolders[SpecialFolder::Videos]      = { "Videos", video };
+        specialFolders[SpecialFolder::LocalVideos] = { "Local Videos", video };
+    }
+
+    roots.push_back({ "/ (root)", "/" });
 }
 
 static std::filesystem::path current_application_path;
