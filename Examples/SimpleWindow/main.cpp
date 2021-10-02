c596ce3702b0c763892bcc8baebf135c86e56c4b
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2021-10-02 08:44:52 +0300

    Simple example done in two ways

--- Examples/SimpleWindow/main.cpp
+++ Examples/SimpleWindow/main.cpp
@@ -4,28 +4,20 @@ using namespace AppCUI;
 using namespace AppCUI::Application;
 using namespace AppCUI::Controls;
 
+#define USING_A_CLASS_IMPLEMENTATION
+
+#ifdef USING_A_CLASS_IMPLEMENTATION
 class SimpleWin : public AppCUI::Controls::Window
 {
-    Label l1;
-
   public:
     SimpleWin()
     {
-        this->Create("Test", "d:c,w:40,h:10");
-        l1.Create(
-              this,
+        Window::Init("Test", "d:c,w:40,h:10", WindowFlags::None);
+        Label::Create(
+              *this,
               "This is a simple Window example that creates a 40x20 fixed window that  will close if you click on the "
               "[X]  button from the bottom-top corner.",
               "x:1,y:1,w:36,h:5");
-    }
-    bool OnEvent(Control*, Event eventType, int) override
-    {
-        if (eventType == Event::WindowClose)
-        {
-            Application::Close();
-            return true;
-        }
-        return false;
     }
 };
 int main()
@@ -36,3 +28,20 @@ int main()
     Application::Run();
     return 0;
 }
+#else
+// No class method
+int main()
+{
+    if (!Application::Init())
+        return 1;
+    auto wnd = Window::Create("Test", "d:c,w:40,h:10");
+    Label::Create(
+          *wnd,
+          "This is a simple Window example that creates a 40x20 fixed window that  will close if you click on the "
+          "[X]  button from the bottom-top corner.",
+          "x:1,y:1,w:36,h:5");
+    Application::AddWindow(std::move(wnd));
+    Application::Run();
+    return 0;
+}
+#endif
