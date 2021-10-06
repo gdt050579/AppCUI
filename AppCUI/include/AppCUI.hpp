556c669046694db1ae4cc0319ffab7289595a958
Denis Rozimovschii <rozimovschii.denis@gmail.com>
2021-10-07 02:20:27 +0300

    Added complex ascii video rendering with autoredraw

--- AppCUI/include/AppCUI.hpp
+++ AppCUI/include/AppCUI.hpp
@@ -3032,6 +3032,7 @@ namespace Application
         FixedSize           = 0x0010,
         LoadSettingsFile    = 0x0020,
         AutoHotKeyForWindow = 0x0040,
+        AutoRedraw          = 0x0080,
     };
 
     enum class CharacterSize : unsigned int
