5c316421cbd13deca58dc8c1ca7494763939b45d
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2021-10-08 00:10:30 +0300

    SDL fix

--- AppCUI/src/Terminal/SDLTerminal/SDLTerminalScreen.cpp
+++ AppCUI/src/Terminal/SDLTerminal/SDLTerminalScreen.cpp
@@ -176,7 +176,7 @@ bool SDLTerminal::initScreen(const InitializationData& initData)
           "Fail to create the original screen canvas of %d x %d size",
           widthInChars,
           heightInChars);
-    autoRedraw = (initData.Flags & InitializationFlags::AutoRedraw) != InitializationFlags::None;
+    autoRedraw = (initData.Flags & InitializationFlags::EnableFPSMode) != InitializationFlags::None;
     return true;
 }
 
