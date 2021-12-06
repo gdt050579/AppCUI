e59395df2db7ba862e7de6b635dbca15c942196a
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2021-12-06 23:33:39 +0200

    add fixed type integers

--- AppCUI/include/AppCUI.hpp
+++ AppCUI/include/AppCUI.hpp
@@ -186,6 +186,19 @@ inline void Unused(...)
 
 namespace AppCUI
 {
+// basic types
+using uint64 = uint64_t;
+using uint32 = uint32_t;
+using uint16 = uint16_t;
+using uint8 = uint8_t;
+using int64 = int64_t;
+using int32 = int32_t;
+using int16 = int16_t;
+using int8 = int8_t;
+using char8 = char8_t;
+using char16 = char16_t;
+
+
 namespace StdIncludes
 {
     using std::initializer_list;
