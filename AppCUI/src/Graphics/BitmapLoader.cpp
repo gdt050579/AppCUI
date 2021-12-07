546f7eb4b1c7febfa0ae22b275f7bb810809685a
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2021-12-07 08:39:36 +0200

    uint32_t to uint32

--- AppCUI/src/Graphics/BitmapLoader.cpp
+++ AppCUI/src/Graphics/BitmapLoader.cpp
@@ -3,29 +3,29 @@
 namespace AppCUI::Graphics
 {
 #pragma pack(push, 1)
-constexpr uint16 BITMAP_WINDOWS_MAGIC               = 0x4D42;
-constexpr uint32_t BITMAP_COMPRESSION_METHID_BI_RGB = 0;
+constexpr uint16 BITMAP_WINDOWS_MAGIC             = 0x4D42;
+constexpr uint32 BITMAP_COMPRESSION_METHID_BI_RGB = 0;
 struct BMP_Header
 {
     uint16 magic;
-    uint32_t size;
+    uint32 size;
     uint16 reserved_1;
     uint16 reserved_2;
-    uint32_t pixelOffset;
+    uint32 pixelOffset;
 };
 struct BMP_InfoHeader
 {
-    uint32_t sizeOfHeader;
-    uint32_t width;
-    uint32_t height;
+    uint32 sizeOfHeader;
+    uint32 width;
+    uint32 height;
     uint16 colorPlanes;
     uint16 bitsPerPixel;
-    uint32_t comppresionMethod;
-    uint32_t imageSize;
-    uint32_t horizontalResolution;
-    uint32_t verticalResolution;
-    uint32_t numberOfColors;
-    uint32_t numberOfImportantColors;
+    uint32 comppresionMethod;
+    uint32 imageSize;
+    uint32 horizontalResolution;
+    uint32 verticalResolution;
+    uint32 numberOfColors;
+    uint32 numberOfImportantColors;
 };
 #pragma pack(pop)
 
@@ -33,17 +33,17 @@ struct DIBPaintBuffer
 {
     const uint8* px;
     const uint8* end;
-    const uint32_t* colorTable;
-    uint32_t width;
-    uint32_t height;
+    const uint32* colorTable;
+    uint32 width;
+    uint32 height;
     const BMP_InfoHeader* header;
 };
 
 bool Paint_32bits_DIB(Image& img, DIBPaintBuffer& d)
 {
-    uint32_t x          = 0;
-    uint32_t y          = d.height - 1;
-    uint32_t rowPadding = 0;
+    uint32 x          = 0;
+    uint32 y          = d.height - 1;
+    uint32 rowPadding = 0;
     while (d.px + 4 <= d.end)
     {
         CHECK(img.SetPixel(x, y, Pixel(d.px[0], d.px[1], d.px[2])),
@@ -66,9 +66,9 @@ bool Paint_32bits_DIB(Image& img, DIBPaintBuffer& d)
 }
 bool Paint_24bits_DIB(Image& img, DIBPaintBuffer& d)
 {
-    uint32_t x          = 0;
-    uint32_t y          = d.height - 1;
-    uint32_t rowPadding = (4 - ((d.width * 3) & 3)) & 3;
+    uint32 x          = 0;
+    uint32 y          = d.height - 1;
+    uint32 rowPadding = (4 - ((d.width * 3) & 3)) & 3;
     while (d.px + 3 <= d.end)
     {
         CHECK(img.SetPixel(x, y, Pixel(d.px[0], d.px[1], d.px[2])),
@@ -91,9 +91,9 @@ bool Paint_24bits_DIB(Image& img, DIBPaintBuffer& d)
 }
 bool Paint_256color_DIB(Image& img, DIBPaintBuffer& d)
 {
-    uint32_t x          = 0;
-    uint32_t y          = d.height - 1;
-    uint32_t rowPadding = (4 - (d.width & 3)) & 3;
+    uint32 x          = 0;
+    uint32 y          = d.height - 1;
+    uint32 rowPadding = (4 - (d.width & 3)) & 3;
     while (d.px <= d.end)
     {
         CHECK(img.SetPixel(x, y, Pixel(d.colorTable[*d.px])), false, "Fail to set pixel on %u,%u coordonates", x, y);
@@ -112,15 +112,15 @@ bool Paint_256color_DIB(Image& img, DIBPaintBuffer& d)
 }
 bool Paint_16color_DIB(Image& img, DIBPaintBuffer& d)
 {
-    uint32_t x          = 0;
-    uint32_t y          = d.height - 1;
-    uint32_t pxWidth    = (d.width & 1) + (d.width >> 1); // for 3 the result is 2, for 8 the result is 4
-    uint32_t rowPadding = (4 - (pxWidth & 3)) & 3;
+    uint32 x          = 0;
+    uint32 y          = d.height - 1;
+    uint32 pxWidth    = (d.width & 1) + (d.width >> 1); // for 3 the result is 2, for 8 the result is 4
+    uint32 rowPadding = (4 - (pxWidth & 3)) & 3;
     while (d.px <= d.end)
     {
         auto val = *d.px;
 
-        for (unsigned int cnt = 0; cnt < 2; cnt++) // two colors per byte
+        for (uint32 cnt = 0; cnt < 2; cnt++) // two colors per byte
         {
             CHECK(img.SetPixel(x, y, Pixel(d.colorTable[(val & 0xF0) >> 4])),
                   false,
@@ -145,16 +145,16 @@ bool Paint_16color_DIB(Image& img, DIBPaintBuffer& d)
 }
 bool Paint_monochrome_DIB(Image& img, DIBPaintBuffer& d)
 {
-    uint32_t x          = 0;
-    uint32_t y          = d.height - 1;
-    uint32_t pxWidth    = (d.width & 7) == 0 ? (d.width >> 8) : (d.width >> 8) + 1;
-    uint32_t rowPadding = (4 - (pxWidth & 3)) & 3;
+    uint32 x          = 0;
+    uint32 y          = d.height - 1;
+    uint32 pxWidth    = (d.width & 7) == 0 ? (d.width >> 8) : (d.width >> 8) + 1;
+    uint32 rowPadding = (4 - (pxWidth & 3)) & 3;
     Color c;
     while (d.px <= d.end)
     {
         auto val = *d.px;
 
-        for (unsigned int cnt = 0; cnt < 8; cnt++) // two colors per byte
+        for (uint32 cnt = 0; cnt < 8; cnt++) // two colors per byte
         {
             if (val & 0x80)
                 c = Color::White;
@@ -177,12 +177,12 @@ bool Paint_monochrome_DIB(Image& img, DIBPaintBuffer& d)
     }
     RETURNERROR(false, "Premature end of bitmap buffer !");
 }
-bool LoadDIBToImage(Image& img, const uint8* buffer, unsigned int size, bool isIcon)
+bool LoadDIBToImage(Image& img, const uint8* buffer, uint32 size, bool isIcon)
 {
     CHECK(size > sizeof(BMP_InfoHeader),
           false,
           "At least %u bytes must be allocated to read a BItmap DIB header",
-          (unsigned int) sizeof(BMP_InfoHeader));
+          (uint32) sizeof(BMP_InfoHeader));
     auto* h = reinterpret_cast<const BMP_InfoHeader*>(buffer);
     CHECK(h->sizeOfHeader == 40, false, "Invalid `sizeOfHeade` value for DIB header");
     CHECK(h->comppresionMethod == BITMAP_COMPRESSION_METHID_BI_RGB,
@@ -222,12 +222,12 @@ bool LoadDIBToImage(Image& img, const uint8* buffer, unsigned int size, bool isI
     case 4:
         // set color table
         dpb.px += 64; // 16 entries x 4 (RGBA)
-        dpb.colorTable = reinterpret_cast<const uint32_t*>(buffer + sizeof(BMP_InfoHeader));
+        dpb.colorTable = reinterpret_cast<const uint32*>(buffer + sizeof(BMP_InfoHeader));
         return Paint_16color_DIB(img, dpb);
     case 8:
         // set color table
         dpb.px += 1024; // 256 entries x 4 (RGBA)
-        dpb.colorTable = reinterpret_cast<const uint32_t*>(buffer + sizeof(BMP_InfoHeader));
+        dpb.colorTable = reinterpret_cast<const uint32*>(buffer + sizeof(BMP_InfoHeader));
         return Paint_256color_DIB(img, dpb);
     case 24:
         return Paint_24bits_DIB(img, dpb);
@@ -236,17 +236,17 @@ bool LoadDIBToImage(Image& img, const uint8* buffer, unsigned int size, bool isI
     }
     RETURNERROR(false, "Paint method for %d bits/pixels is not implemeted !", h->bitsPerPixel);
 }
-bool LoadBMPToImage(Image& img, const uint8* buffer, unsigned int size)
+bool LoadBMPToImage(Image& img, const uint8* buffer, uint32 size)
 {
     CHECK(buffer, false, "Expecting a valid (non-null) buffer");
     CHECK(size > sizeof(BMP_Header),
           false,
           "At least %u bytes must be allocated to read a BItmap header",
-          (unsigned int) sizeof(BMP_Header));
+          (uint32) sizeof(BMP_Header));
     auto* h = reinterpret_cast<const BMP_Header*>(buffer);
     CHECK(h->magic == BITMAP_WINDOWS_MAGIC, false, "Invalid magic --> expecting 'BM'");
     CHECK(h->size <= size, false, "Size field in bitmap is invalid (%u), should be at least %u", h->size, size);
     // all good
-    return LoadDIBToImage(img, buffer + sizeof(BMP_Header), size - (unsigned int) sizeof(BMP_Header), false);
+    return LoadDIBToImage(img, buffer + sizeof(BMP_Header), size - (uint32) sizeof(BMP_Header), false);
 }
 } // namespace AppCUI::Graphics
