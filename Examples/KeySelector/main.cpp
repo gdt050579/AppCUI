d198d5416c8391881aad9c01ac07ec6f354ad125
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2021-12-22 09:21:33 +0200

    example for KeySelector

---
+++ Examples/KeySelector/main.cpp
@@ -0,0 +1,24 @@
+#include "AppCUI.hpp"
+
+using namespace AppCUI;
+using namespace AppCUI::Application;
+using namespace AppCUI::Controls;
+
+
+class ExampleWin : public Window
+{
+  public:
+    ExampleWin() : Window("Example", "d:c,w:40,h:10", WindowFlags::None)
+    {
+        Factory::KeySelector::Create(this, "x:1,y:1,w:38");
+        Factory::Button::Create(this, "A button", "d:b,w:20", 123);
+    }
+};
+int main()
+{
+    if (!Application::Init())
+        return 1;
+    Application::AddWindow(std::make_unique<ExampleWin>());
+    Application::Run();
+    return 0;
+}
