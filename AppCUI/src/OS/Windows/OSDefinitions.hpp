15fb261054e6a3d2e4180e295aae7ba0ff32a220
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2022-01-11 08:39:14 +0200

    Remove unnecesary macros from OSDefinitions.hpp

--- AppCUI/src/OS/Windows/OSDefinitions.hpp
+++ AppCUI/src/OS/Windows/OSDefinitions.hpp
@@ -1,22 +1,4 @@
 #pragma once
 
-#include <windows.h>
-#include <Shlobj.h>
 
-#define CHARACTER_INFORMATION CHAR_INFO
-#define SET_CHARACTER_VALUE(ptrCharInfo, value)                                                                        \
-    {                                                                                                                  \
-        (ptrCharInfo)->Char.UnicodeChar = (value);                                                                     \
-    }
-#define SET_CHARACTER_COLOR(ptrCharInfo, color)                                                                        \
-    {                                                                                                                  \
-        (ptrCharInfo)->Attributes = (color);                                                                           \
-    }
-#define SET_CHARACTER(ptrCharInfo, value, color)                                                                       \
-    {                                                                                                                  \
-        (ptrCharInfo)->Char.UnicodeChar = (value);                                                                     \
-        (ptrCharInfo)->Attributes       = (color);                                                                     \
-    }
-#define GET_CHARACTER_COLOR(ptrCharInfo) ((ptrCharInfo)->Attributes)
-#define GET_CHARACTER_VALUE(ptrCharInfo) ((ptrCharInfo)->Char.UnicodeChar)
 
