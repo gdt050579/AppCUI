e3f62d7991ef3c7d0883dd8087c4a8b9dd89f74e
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2022-01-24 09:21:39 +0200

    theme editor window & example

--- AppCUI/src/Dialogs/ThemeEditor.cpp
+++ AppCUI/src/Dialogs/ThemeEditor.cpp
@@ -1,8 +1,27 @@
 #include "AppCUI.hpp"
 
+using namespace AppCUI::Controls;
+using namespace AppCUI::Graphics;
+
 namespace AppCUI::Dialogs
 {
+class ThemeEditorDialog : public Window
+{
+    AppCUI::Application::Config cfg;
+  public:
+    ThemeEditorDialog(const AppCUI::Application::Config& configObject) : Window("Theme editor", "d:c,w:80,h:24",WindowFlags::Sizeable)
+    {
+        cfg = configObject;
+    }
+};
+
 void ThemeEditor::Show()
 {
+    auto* cfg = Application::GetAppConfig();
+    if (cfg)
+    {
+        ThemeEditorDialog dlg(*cfg);
+        dlg.Show();
+    }
 }
-}
+} // namespace AppCUI::Dialogs
