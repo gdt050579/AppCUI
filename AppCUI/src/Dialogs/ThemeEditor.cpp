0c90f5b908a35ca84c0e7139b8666ffdbec91e31
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2022-01-29 11:22:13 +0200

    Remove selected component from ParentMenu as it is not used

--- AppCUI/src/Dialogs/ThemeEditor.cpp
+++ AppCUI/src/Dialogs/ThemeEditor.cpp
@@ -41,16 +41,12 @@ enum class PropID : uint32
     // parent menu
     ParentMenuTextNormal,
     ParentMenuTextHovered,
-    ParentMenuTextSelected,
     ParentMenuHotKeyNormal,
     ParentMenuHotKeyHovered,
-    ParentMenuHotKeySelected,
     ParentMenuShortCutNormal,
     ParentMenuShortCutHovered,
-    ParentMenuShortCutSelected,
     ParentMenuSymbolNormal,
     ParentMenuSymbolHovered,
-    ParentMenuSymbolSelected,
     ParentMenuInactive,
 
 };
@@ -230,26 +226,17 @@ class ConfigProperty : public PropertiesInterface
         case PropID::ParentMenuTextHovered:
             value = obj.ParentMenu.Text.Hovered;
             return true;
-        case PropID::ParentMenuTextSelected:
-            value = obj.ParentMenu.Text.PressedOrSelected;
-            return true;
         case PropID::ParentMenuHotKeyNormal:
             value = obj.ParentMenu.HotKey.Normal;
             return true;
         case PropID::ParentMenuHotKeyHovered:
             value = obj.ParentMenu.HotKey.Hovered;
             return true;
-        case PropID::ParentMenuHotKeySelected:
-            value = obj.ParentMenu.HotKey.PressedOrSelected;
-            return true;
         case PropID::ParentMenuShortCutNormal:
             value = obj.ParentMenu.ShortCut.Normal;
             return true;
         case PropID::ParentMenuShortCutHovered:
             value = obj.ParentMenu.ShortCut.Hovered;
-            return true;
-        case PropID::ParentMenuShortCutSelected:
-            value = obj.ParentMenu.ShortCut.PressedOrSelected;
             return true;
         case PropID::ParentMenuInactive:
             value = obj.ParentMenu.Text.Inactive.Foreground;
@@ -259,9 +246,6 @@ class ConfigProperty : public PropertiesInterface
             return true;
         case PropID::ParentMenuSymbolHovered:
             value = obj.ParentMenu.Symbol.Hovered.Foreground;
-            return true;
-        case PropID::ParentMenuSymbolSelected:
-            value = obj.ParentMenu.Symbol.PressedOrSelected.Foreground;
             return true;
         }
         return false;
@@ -335,20 +319,12 @@ class ConfigProperty : public PropertiesInterface
             obj.ParentMenu.Symbol.Hovered.Background = obj.ParentMenu.Text.Hovered.Background;
             obj.ParentMenu.Symbol.Hovered.Background = obj.ParentMenu.Text.Hovered.Background;
             return true;
-        case PropID::ParentMenuTextSelected:
-            obj.ParentMenu.Text.PressedOrSelected              = std::get<ColorPair>(value);
-            obj.ParentMenu.Symbol.PressedOrSelected.Background = obj.ParentMenu.Text.PressedOrSelected.Background;
-            obj.ParentMenu.Symbol.PressedOrSelected.Background = obj.ParentMenu.Text.PressedOrSelected.Background;
-            return true;
         case PropID::ParentMenuHotKeyNormal:
             obj.ParentMenu.HotKey.Normal              = std::get<ColorPair>(value);
             obj.ParentMenu.HotKey.Inactive.Background = obj.ParentMenu.HotKey.Normal.Background;
             return true;
         case PropID::ParentMenuHotKeyHovered:
             obj.ParentMenu.HotKey.Hovered = std::get<ColorPair>(value);
-            return true;
-        case PropID::ParentMenuHotKeySelected:
-            obj.ParentMenu.HotKey.PressedOrSelected = std::get<ColorPair>(value);
             return true;
         case PropID::ParentMenuShortCutNormal:
             obj.ParentMenu.ShortCut.Normal              = std::get<ColorPair>(value);
@@ -357,17 +333,11 @@ class ConfigProperty : public PropertiesInterface
         case PropID::ParentMenuShortCutHovered:
             obj.ParentMenu.ShortCut.Hovered = std::get<ColorPair>(value);
             return true;
-        case PropID::ParentMenuShortCutSelected:
-            obj.ParentMenu.ShortCut.PressedOrSelected = std::get<ColorPair>(value);
-            return true;
         case PropID::ParentMenuSymbolNormal:
             obj.ParentMenu.Symbol.Normal.Foreground = std::get<Color>(value);
             return true;
         case PropID::ParentMenuSymbolHovered:
             obj.ParentMenu.Symbol.Hovered.Foreground = std::get<Color>(value);
-            return true;
-        case PropID::ParentMenuSymbolSelected:
-            obj.ParentMenu.Symbol.PressedOrSelected.Foreground = std::get<Color>(value);
             return true;
         case PropID::ParentMenuInactive:
             obj.ParentMenu.Text.Inactive.Foreground     = std::get<Color>(value);
@@ -411,16 +381,12 @@ class ConfigProperty : public PropertiesInterface
             // parent menu
             { PT(PropID::ParentMenuTextNormal), CAT(CatID::ParentMenu), "Text (normal)", PropertyType::ColorPair },
             { PT(PropID::ParentMenuTextHovered), CAT(CatID::ParentMenu), "Text (hovered)", PropertyType::ColorPair },
-            { PT(PropID::ParentMenuTextSelected), CAT(CatID::ParentMenu), "Text (selected)", PropertyType::ColorPair },
             { PT(PropID::ParentMenuHotKeyNormal), CAT(CatID::ParentMenu), "HotKey (normal)", PropertyType::ColorPair },
             { PT(PropID::ParentMenuHotKeyHovered), CAT(CatID::ParentMenu), "HotKey (hovered)", PropertyType::ColorPair },
-            { PT(PropID::ParentMenuHotKeySelected), CAT(CatID::ParentMenu), "HotKey (selected)", PropertyType::ColorPair },
             { PT(PropID::ParentMenuShortCutNormal), CAT(CatID::ParentMenu), "ShortCut (normal)", PropertyType::ColorPair },
             { PT(PropID::ParentMenuShortCutHovered), CAT(CatID::ParentMenu), "ShortCut (hovered)", PropertyType::ColorPair },
-            { PT(PropID::ParentMenuShortCutSelected), CAT(CatID::ParentMenu), "ShortCut (selected)", PropertyType::ColorPair },
             { PT(PropID::ParentMenuInactive), CAT(CatID::ParentMenu), "Inactive", PropertyType::Color },
             { PT(PropID::ParentMenuSymbolNormal), CAT(CatID::ParentMenu), "Symbols (normal)", PropertyType::Color },
-            { PT(PropID::ParentMenuSymbolSelected), CAT(CatID::ParentMenu), "Symbols (selected)", PropertyType::Color },
             { PT(PropID::ParentMenuSymbolHovered), CAT(CatID::ParentMenu), "Symbols (hovered)", PropertyType::Color },
 
         };
