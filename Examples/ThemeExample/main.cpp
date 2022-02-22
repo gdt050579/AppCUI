cadb6b52588fb513dae02e0ad7e8b624f849da54
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2022-02-22 21:25:55 +0200

    Add radiobox to select a specific theme

--- Examples/ThemeExample/main.cpp
+++ Examples/ThemeExample/main.cpp
@@ -5,12 +5,20 @@ using namespace AppCUI::Application;
 using namespace AppCUI::Controls;
 using namespace AppCUI::Input;
 
+constexpr int CMD_CHANGE_THEME_DEFAULT = 0;
+constexpr int CMD_CHANGE_THEME_DARK    = 1;
+constexpr int CMD_CHANGE_THEME_LIGHT   = 2;
+
 class ExampleWin : public Window
 {
   public:
     ExampleWin() : Window("Test", "d:c,w:70,h:24", WindowFlags::None)
     {
         Factory::Label::Create(this, "This is an example of how themes work on AppCUI", "x:8,y:1,w:66");
+        auto p = Factory::Panel::Create(this, "Themes", "x:2,y:3,w:20,h:5");
+        Factory::RadioBox::Create(p, "Default", "x:1,y:0,w:15", 100, CMD_CHANGE_THEME_DEFAULT)->SetChecked(true);
+        Factory::RadioBox::Create(p, "Dark", "x:1,y:1,w:15", 100, CMD_CHANGE_THEME_DARK);
+        Factory::RadioBox::Create(p, "Light", "x:1,y:2,w:15", 100, CMD_CHANGE_THEME_LIGHT);
     }
     bool OnUpdateCommandBar(CommandBar& cmd) override
     {
@@ -23,7 +31,10 @@ class ExampleWin : public Window
 };
 int main()
 {
-    if (!Application::Init(InitializationFlags::Menu | InitializationFlags::CommandBar))
+    InitializationData initData;
+    initData.Flags = InitializationFlags::Menu | InitializationFlags::CommandBar;
+    initData.Theme = ThemeType::Default;
+    if (!Application::Init(initData))
         return 1;
     Application::AddWindow(std::make_unique<ExampleWin>());
     Application::Run();
