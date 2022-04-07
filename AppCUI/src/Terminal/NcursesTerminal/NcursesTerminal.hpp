6754178aaa242747c4ce08ce4daa453757a28583
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2022-04-07 10:10:19 +0300

    Add method OnFlushToScreen(Rect..) to NCurses terminal

--- AppCUI/src/Terminal/NcursesTerminal/NcursesTerminal.hpp
+++ AppCUI/src/Terminal/NcursesTerminal/NcursesTerminal.hpp
@@ -61,6 +61,7 @@ namespace Internal
         virtual bool OnInit(const Application::InitializationData& initData) override;
         virtual void OnUninit() override;
         virtual void OnFlushToScreen() override;
+	virtual void OnFlushToScreen(const Graphics::Rect& r) override;
         virtual bool OnUpdateCursor() override;
         virtual void GetSystemEvent(Internal::SystemEvent& evnt) override;
         virtual bool IsEventAvailable() override;
