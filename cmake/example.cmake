28697756e2b683a235dfead8de09f826b8440ab4
Gheorghita MUTU <gheorghitamutu@gmail.com>
2022-07-03 19:48:46 +0300

    ^ [Build] - global LTO
    Signed-off-by: Gheorghita MUTU <gheorghitamutu@gmail.com>

--- cmake/example.cmake
+++ cmake/example.cmake
@@ -1,6 +1,16 @@
 function (create_example example_name)
 	set(PROJECT_NAME ${example_name})
 	
+	include(CheckIPOSupported)
+	check_ipo_supported(RESULT supported OUTPUT error)
+	
+	if( supported )
+		message(STATUS "${PROJECT_NAME} => IPO / LTO enabled")
+		set(CMAKE_INTERPROCEDURAL_OPTIMIZATION TRUE)
+	else()
+		message(STATUS "${PROJECT_NAME} => IPO / LTO not supported: <${error}>")
+	endif()
+	
 	include_directories(../../AppCUI/include)
 	
 	add_executable(${PROJECT_NAME} main.cpp)
@@ -8,15 +18,5 @@ function (create_example example_name)
 	
 	target_link_libraries(${PROJECT_NAME} PRIVATE AppCUI)
 	set_target_properties(${PROJECT_NAME} PROPERTIES FOLDER "Examples")
-	
-	include(CheckIPOSupported)
-	check_ipo_supported(RESULT supported OUTPUT error)
-	
-	if( supported )
-		message(STATUS "${PROJECT_NAME} => IPO / LTO enabled")
-		set_property(TARGET ${PROJECT_NAME} PROPERTY INTERPROCEDURAL_OPTIMIZATION TRUE)
-	else()
-		message(STATUS "${PROJECT_NAME} => IPO / LTO not supported: <${error}>")
-	endif()
 endfunction()
 
