8c7d0152b8ffed3378c3a75ce014cc71ddda92db
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2022-07-09 11:01:15 +0300

    Add example for testing scripts

---
+++ Examples/TestScript/main.cpp
@@ -0,0 +1,26 @@
+#include "AppCUI.hpp"
+
+using namespace AppCUI;
+using namespace AppCUI::Application;
+using namespace AppCUI::Controls;
+
+class SimpleWin : public Window
+{
+  public:
+    SimpleWin() : Window("Test", "d:c,w:40,h:10", WindowFlags::None)
+    {
+        Factory::Label::Create(
+              this,
+              "This is a simple Window example that creates a 40x20 fixed window that  will close if you click on the "
+              "[X]  button from the bottom-top corner.",
+              "x:1,y:1,w:36,h:5");
+    }
+};
+int main()
+{
+    if (!Application::Init())
+        return 1;
+    Application::AddWindow(std::make_unique<SimpleWin>());
+    Application::Run();
+    return 0;
+}
