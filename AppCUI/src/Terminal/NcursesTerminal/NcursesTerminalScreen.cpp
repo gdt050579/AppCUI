4c869a0fa4d5a3b0cc6613f4ac014361ea7d3826
Gheorghita Mutu <gheorghitamutu@gmail.com>
2022-09-03 19:34:15 +0300

    Update NcursesTerminalScreen.cpp

--- AppCUI/src/Terminal/NcursesTerminal/NcursesTerminalScreen.cpp
+++ AppCUI/src/Terminal/NcursesTerminal/NcursesTerminalScreen.cpp
@@ -47,7 +47,7 @@ void NcursesTerminal::OnFlushToScreen()
         {
             const Graphics::Character ch = charsBuffer[y * width + x];
 
-            cchar_t t = { 0, { ch.Code, 0 }, {} };
+            cchar_t t = { 0, { ch.Code, 0 } };
             colors.SetColor(ch.Color.Foreground, ch.Color.Background);
             mvadd_wch(y, x, &t);
             colors.UnsetColor(ch.Color.Foreground, ch.Color.Background);
