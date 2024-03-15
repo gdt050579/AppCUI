fa85b8e62d80b72f50a789a2e9ed1e3e357b59fc
Gheorghita Mutu <gheorghitamutu@gmail.com>
2024-03-15 12:24:39 +0200

    [TreeView] + add tooltip on mouse over search area
    Signed-off-by: Gheorghita Mutu <gheorghitamutu@gmail.com>

--- AppCUI/src/Controls/TreeView.cpp
+++ AppCUI/src/Controls/TreeView.cpp
@@ -98,7 +98,7 @@ void TreeView::Paint(Graphics::Renderer& renderer)
 
     if (cc->Focused)
     {
-        if ((cc->Layout.Width > (int)TreeSearchBarWidth) && (cc->filter.mode != TreeControlContext::FilterMode::None))
+        if ((cc->Layout.Width > (int) TreeSearchBarWidth) && (cc->filter.mode != TreeControlContext::FilterMode::None))
         {
             renderer.FillHorizontalLine(1, cc->Layout.Height - 1, TreeSearchBarWidth, ' ', cc->Cfg->SearchBar.Normal);
 
@@ -598,6 +598,17 @@ bool TreeView::OnMouseWheel(int /*x*/, int /*y*/, Input::MouseWheel direction, I
     }
 
     return false;
+}
+
+bool TreeView::OnMouseOver(int x, int y)
+{
+    CHECKRET(Context != nullptr, "");
+    const auto cc = reinterpret_cast<TreeControlContext*>(Context);
+    if (cc->IsMouseOnSearchField(x, y))
+    {
+        this->ShowToolTip("Search/Filter");
+    }
+    return true;
 }
 
 void TreeView::OnUpdateScrollBars()
@@ -1307,7 +1318,8 @@ bool TreeControlContext::IsMouseOnItem(int x, int y) const
 
 bool TreeControlContext::IsMouseOnBorder(int x, int y) const
 {
-    return ((x == 0) || (x == Layout.Width - (int)BorderOffset) || (y == 0) || (y == Layout.Width - (int)BorderOffset));
+    return (
+          (x == 0) || (x == Layout.Width - (int) BorderOffset) || (y == 0) || (y == Layout.Width - (int) BorderOffset));
 }
 
 bool TreeControlContext::IsMouseOnColumnHeader(int x, int y)
@@ -1356,7 +1368,7 @@ bool TreeControlContext::IsMouseOnSearchField(int x, int y) const
     {
         if (y == Layout.Height - 1)
         {
-            if ((x > 0) && (x < (int)TreeSearchBarWidth))
+            if ((x > 0) && (x < (int) TreeSearchBarWidth))
             {
                 return true;
             }
@@ -1370,7 +1382,8 @@ bool TreeControlContext::AdjustElementsOnResize(const int /*newWidth*/, const in
 {
     CHECK(AdjustItemsBoundsOnResize(), false, "");
     const auto columnsSize = this->Header.GetColumnsCount();
-    //const uint32 width = (static_cast<uint32>(Layout.Width)) / static_cast<uint32>(std::max<uint32>(columnsSize, 1U));
+    // const uint32 width = (static_cast<uint32>(Layout.Width)) / static_cast<uint32>(std::max<uint32>(columnsSize,
+    // 1U));
 
     uint32 xPreviousColumn       = 0;
     uint32 widthOfPreviousColumn = 0;
@@ -1382,7 +1395,7 @@ bool TreeControlContext::AdjustElementsOnResize(const int /*newWidth*/, const in
         widthOfPreviousColumn = col.width;
     }
 
-    if (Layout.Width <= (int)TreeScrollbarLeftOffset)
+    if (Layout.Width <= (int) TreeScrollbarLeftOffset)
     {
         if (hidSearchBarOnResize == false)
         {
