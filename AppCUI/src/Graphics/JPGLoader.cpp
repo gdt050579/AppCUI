80a654f0d1a89120e6252dfe62a3ec415e9f815b
Gheorghita Mutu <gheorghitamutu@gmail.com>
2024-07-25 14:30:39 +0300

    [JPEG] # fix linkage
    Signed-off-by: Gheorghita Mutu <gheorghitamutu@gmail.com>

--- AppCUI/src/Graphics/JPGLoader.cpp
+++ AppCUI/src/Graphics/JPGLoader.cpp
@@ -5,43 +5,38 @@ namespace AppCUI::Graphics
 {
 bool LoadJPGToImage(Image& img, const uint8* imageBuffer, uint32 size)
 {
-    tjhandle tjInstance = tjInitDecompress();
-    if (!tjInstance)
-    {
-        RETURNERROR(false, "Failed to initialize TurboJPEG decompressor");
-    }
+    std::unique_ptr<void, void (*)(void*)> handle(
+          reinterpret_cast<void*>(tjInitDecompress()), [](void* p) { tjDestroy(reinterpret_cast<tjhandle>(p)); });
+    CHECK(handle != nullptr, false, "Failed to initialize TurboJPEG decompressor!");
 
     int width, height, jpegSubsamp;
-    if (tjDecompressHeader2(
-              tjInstance,
-              const_cast<unsigned char*>(reinterpret_cast<const unsigned char*>(imageBuffer)),
-              size,
-              &width,
-              &height,
-              &jpegSubsamp) != 0)
-    {
-        tjDestroy(tjInstance);
-        RETURNERROR(false, "Failed to read JPEG header: %s", tjGetErrorStr());
-    }
+    CHECK(tjDecompressHeader2(
+                handle.get(),
+                const_cast<unsigned char*>(reinterpret_cast<const unsigned char*>(imageBuffer)),
+                size,
+                &width,
+                &height,
+                &jpegSubsamp) == 0,
+          false,
+          "Failed to read JPEG header: %s",
+          tjGetErrorStr());
 
     CHECK(img.Create(width, height), false, "Fail to create a %ux%u size image", width, height);
 
-    if (tjDecompress2(
-              tjInstance,
-              const_cast<unsigned char*>(reinterpret_cast<const unsigned char*>(imageBuffer)),
-              size,
-              reinterpret_cast<unsigned char*>(img.GetPixelsBuffer()),
-              width,
-              0 /* pitch */,
-              height,
-              TJPF_RGB,
-              TJFLAG_FASTDCT) != 0)
-    {
-        tjDestroy(tjInstance);
-        RETURNERROR(false, "Failed to decompress JPEG image: %s", tjGetErrorStr());
-    }
+    CHECK(tjDecompress2(
+                handle.get(),
+                const_cast<unsigned char*>(reinterpret_cast<const unsigned char*>(imageBuffer)),
+                size,
+                reinterpret_cast<unsigned char*>(img.GetPixelsBuffer()),
+                width,
+                0 /* pitch */,
+                height,
+                TJPF_RGB,
+                TJFLAG_FASTDCT) == 0,
+          false,
+          "Failed to decompress JPEG image: %s",
+          tjGetErrorStr());
 
-    tjDestroy(tjInstance);
     return true;
 }
 } // namespace AppCUI::Graphics
